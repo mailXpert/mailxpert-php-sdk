0cc8575a4a06d01531d33adee1eff8513cb44afc
Jean-Christophe Cuvelier <jcc@artack.ch>
2015-08-24 11:50:19 +0200

    Add Post proxy

--- src/Mailxpert/Request/CustomFieldsChoicesRequest.php
+++ src/Mailxpert/Request/CustomFieldsChoicesRequest.php
@@ -6,13 +6,40 @@
 namespace Mailxpert\Request;
 
 
+use Mailxpert\Exceptions\MailxpertSDKException;
 use Mailxpert\Mailxpert;
 
 class CustomFieldsChoicesRequest
 {
+    /**
+     * @param Mailxpert $mailxpert
+     * @param string    $customFieldId
+     * @param array     $params
+     *
+     * @return \Mailxpert\MailxpertResponse
+     */
     public static function get(Mailxpert $mailxpert, $customFieldId, array $params = [])
     {
         $response = $mailxpert->sendRequest('GET', sprintf('custom_fields/%s/choices', $customFieldId), $params);
+
+        return $response;
+    }
+
+    /**
+     * @param Mailxpert $mailxpert
+     * @param string    $customFieldId
+     * @param array     $params
+     *
+     * @return \Mailxpert\MailxpertResponse
+     * @throws MailxpertSDKException
+     */
+    public static function post(Mailxpert $mailxpert, $customFieldId, array $params)
+    {
+        $response = $mailxpert->sendRequest('GET', sprintf('custom_fields/%s/choices', $customFieldId), $params);
+
+        if (!$response->getHeader('Location')) {
+            throw new MailxpertSDKException('An error occured during the Contact creation.');
+        }
 
         return $response;
     }
